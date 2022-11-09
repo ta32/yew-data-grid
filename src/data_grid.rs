ca60d2275574bb1acc2a583bb384ce2345ed6560
ta32 <26617779+ta32@users.noreply.github.com>
2022-11-09 23:34:13 +1100

    got the data-grid to show data in a table format

--- src/data_grid.rs
+++ src/data_grid.rs
@@ -1,5 +1,4 @@
 use yew::prelude::*;
-use yew::virtual_dom::VNode;
 
 #[derive(Properties, Clone, PartialEq)]
 pub struct Props<T: GridData<ColumnType=U> + PartialEq, U: GridDataColumn<RowType=T> + PartialEq + Copy> {
@@ -8,18 +7,35 @@ pub struct Props<T: GridData<ColumnType=U> + PartialEq, U: GridDataColumn<RowTyp
     pub page_size: usize
 }
 
+const DATA_GRID_STYLE: &'static str = include_str!("data_grid.rs.css");
+
 #[function_component(DataGrid)]
 pub fn data_grid<T: GridData<ColumnType=U> + PartialEq,
                  U: GridDataColumn<RowType=T> + PartialEq + Copy>
                 (props: &Props<T, U>) -> Html {
-    let grid: Html = props.rows.iter().map(|row| props.columns.iter().map(move |col| (row, col))).flatten().map(|(row, col)| {
-        let value = col.get_value(row);
-        html! {
-            <div>{value}</div>
-        }
+    let grid = props.rows.iter().enumerate().map(|(i, row)| {
+        let row_index_str = i.to_string();
+        // row elements
+        let row_values = props.columns.iter().enumerate().map(|(i,col)| {
+            let value = col.get_value(row);
+            let col_index_str = i.to_string();
+            let style = format!("width: 150px; height: 52px;");
+            html! {
+                <div class="yew-data-grid-cell" style={style} row-index={row_index_str.clone()} col-index={col_index_str}>
+                    <div class="yew-data-grid-cell-content">{value}</div>
+                </div>
+            }
+        }).collect::<Html>();
+        let style = format!("height: 52px;");
+        html! (
+            <div class="yew-data-grid-row" style={style} row-index={row_index_str}>
+                {row_values}
+            </div>
+        )
     }).collect::<Html>();
     html!(
-        <div>
+        <div class="yew-data-grid-container">
+            <style>{DATA_GRID_STYLE}</style>
             <h1>{ "data grid" }</h1>
             {grid}
         </div>
@@ -49,10 +65,23 @@ mod tests {
 
     #[test]
     fn example_cross_product() {
-        let rows = vec![1, 2];
+        let rows = vec![1, 2 , 3];
         let cols = vec![4, 5];
         let c = rows.iter().map(|r| cols.iter().map(move |c| (r, c))).flatten();
         let d: Vec<(i32, i32)> = c.map(|(a, b)| (*a, *b)).collect();
-        assert_eq!(d, vec![(1, 4), (1, 5), (2, 4), (2, 5)]);
+        assert_eq!(d, vec![(1, 4), (1, 5), (2, 4), (2, 5), (3, 4), (3, 5)]);
+
+        let rows = vec![4, 5];
+        let cols = vec![1, 2 , 3];
+        let c = rows.iter().map(|r| cols.iter().map(move |c| (r, c))).flatten();
+        let d: Vec<(i32, i32)> = c.map(|(a, b)| (*a, *b)).collect();
+        assert_eq!(d, vec![(4, 1), (4, 2), (4, 3), (5, 1), (5, 2), (5, 3)]);
+
+        // iterator with index (enumerate)
+        let rows = vec![1, 2 , 3];
+        let cols = vec![4, 5];
+        let c = rows.iter().enumerate().map(|(i, r)| cols.iter().map(move |c| (i, r, c))).flatten();
+        let d: Vec<(usize, i32, i32)> = c.map(|(a, b, c)| (a, *b, *c)).collect();
+        assert_eq!(d, vec![(0, 1, 4), (0, 1, 5), (1, 2, 4), (1, 2, 5), (2, 3, 4), (2, 3, 5)]);
     }
 }

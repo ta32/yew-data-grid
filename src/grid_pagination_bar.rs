73ad62e0729c737cf6a30145342a8b030f316123
ta32 <26617779+ta32@users.noreply.github.com>
2023-01-07 18:52:06 +1100

    style the pagination bar

--- src/grid_pagination_bar.rs
+++ src/grid_pagination_bar.rs
@@ -16,13 +16,16 @@ pub struct Props {
 pub fn grid_pagination_bar(i: &Props) -> Html {
     let total_rows = i.props.total_rows;
     let summary = format!("1 - {total_rows} of {total_rows}");
+    const BAR_HEIGHT: i32 = 48;
+    let style = format!("min-height: {BAR_HEIGHT}px; display: flex; flex-direction: row;");
     html! {
-        <div class="yew-grid-pagination-bar">
-            <div class="yew-grid-pagination-bar-summary">{summary}</div>
+        <div class="yew-grid-pagination-bar" style={style}>
             <div class="yew-grid-pagination-bar-controls">
                 <button class="yew-grid-pagination-bar-control-button">{"<"}</button>
+                <button class="yew-grid-pagination-bar-control-button">{"1"}</button>
                 <button class="yew-grid-pagination-bar-control-button">{">"}</button>
             </div>
+            <div class="yew-grid-pagination-bar-summary">{summary}</div>
         </div>
     }
 }
